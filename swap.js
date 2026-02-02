131853e71c002da1747f14ff5efa70e1a004830c
arashik65 <mdashikur6398+@gmail.com>
2026-02-03 04:27:22 +0600

    swaping

---
+++ swap.js
@@ -0,0 +1,15 @@
+let a =5;
+let b =7;
+
+console.log(a,b);
+
+const temp = a;
+a=b;
+console.log(a,b);
+
+//-------------2nd reule 
+let x =5 ;
+let y=7 ;
+console.log(x , y);
+[x , y] =[y , x];
+console.log(x , y);
