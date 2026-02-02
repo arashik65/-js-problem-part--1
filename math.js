12b69fbe349da047ef9d5db66e2ce96e0768182a
arashik65 <mdashikur6398+@gmail.com>
2026-02-03 03:39:48 +0600

    math

---
+++ math.js
@@ -0,0 +1,17 @@
+const min = Math.min(45, 21 , 65, 126, -99)
+console.log(min);
+
+const max = Math.max(45,21,65,99,126, -99);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(5-10));
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+console.log(Math.round(4.55));
+console.log(Math.floor(2.95));
+console.log(Math.ceil(2.00001));
+console.log(Math.ceil(2.45445));
+console.log(Math.random()*10);
+const rand = Math.round(Math.random()*10);
+console.log(rand);
